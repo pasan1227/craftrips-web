cd49e332a378c923e619512113589f71334300cb
pasan1227 <pasanratnayake@gmail.com>
2021-01-01 11:38:39 +0530

    removed index-2

--- js/google-map.js
+++ js/google-map.js
@@ -1,11 +1,10 @@
-
 var google;
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(7.8731, 80.7718);
     // 39.399872
     // -8.224454
     
@@ -43,7 +42,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
+    var addresses = ['Sri Lanka'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
